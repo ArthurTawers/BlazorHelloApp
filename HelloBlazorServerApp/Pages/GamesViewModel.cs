128671d06d4a2330e390c1acea864601744c0c63
goodwomeen <102791569+Goodmeens@users.noreply.github.com>
2022-11-18 19:25:41 +0700

    firstCommit

---
+++ HelloBlazorServerApp/Pages/GamesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HelloBlazorServerApp.Models;
+
+namespace HelloBlazorServerApp.Pages
+{
+    public class GamesViewModel
+    {
+        public List<Game> Games { get; set; } = new List<Game>();
+
+        public async void LoadGamesAsync()
+        {
+            await Task.Delay(3000);
+            Games = new List<Game>
+            {
+                new Game()
+                {
+                    Name = "Farming simulator",
+                    Genre = "simulator",
+                    ReleaseDate = new DateTime(2018,11,19)
+                },
+                new Game()
+                {
+                    Name = "Destroy All Humans!",
+                    Genre = "Action Adventure",
+                    ReleaseDate = new DateTime(2020,07,12)
+                },
+                new Game()
+                {
+                    Name = "The Weather Three",
+                    Genre = "Action/RPG",
+                    ReleaseDate = new DateTime(2015,05,18)
+                },
+            };
+
+            Console.WriteLine(Games.Count);
+        }
+    }
+}
---
+++ HelloBlazorServerApp/Pages/MessangerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloBlazorServerApp.Pages
+{
+    public class MessangerViewModel
+    {
+        public List<(string,DateTime)> Messanges = new List<(string,DateTime)>();
+
+        public void AddMessange(string message,DateTime dt)
+        {
+            Messanges.Add((message,dt));
+        }
+    }
+}
